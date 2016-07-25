127b58b81cf907e5fa3a2b90099adf92bc131e29
flyws <flyws1993@hotmail.com>
2016-07-25 09:06:15 -0700

    Update OAuth.py

--- OAuth.py
+++ OAuth.py
@@ -2,10 +2,10 @@ from twitter import *
 
 def Create_Apps():
 	app = Twitter(auth=OAuth(
-		token = '1123675933-bck1BCvRTkQHII2ahOghKxp9VBLnIZHGH7FkulV', 
-		token_secret ='VTEjd4xlueZag9h0voaZNKBdCBmQX1EwPjEF6ZnWBzYLM', 
-		consumer_key = 'U3ydJHVnlWOY3Cfp71pfZHKgR', 
-		consumer_secret = 'VHuhvk1N7pYtjZXpj9vttqGIoVHHIms94WtItM6rppsKU4JYkE'))
+		token = '-------------------------', 
+		token_secret ='-----------------------', 
+		consumer_key = '---------------------------', 
+		consumer_secret = '-----------------------------'))
 	return app
 
 if __name__ == '__main__':
